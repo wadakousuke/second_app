031eeac366524767144e04d260cf1aef5aeeee22
wadakousuke <100855797+wadakousuke@users.noreply.github.com>
2022-03-10 13:51:08 +0000

    [Add　トップページの追加]

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "604c67d4164646b68ba279303422096f.vfs.cloud9.us-east-1.amazonaws.com"
+
 end
